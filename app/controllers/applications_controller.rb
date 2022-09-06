5bf7fa43c193d9dd3c140e22dddce4a47da0a0ed
Blake McKenzie <blakemckenzie365@gmail.com>
2022-09-06 11:23:13 +0100

    applications controller scaffold

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationsController < ApplicationController
+  before_action :set_pet, only: [:create, :edit, :update]
+
+  def create
+    @application = Booking.new(application_params)
+    @application.pet = @pet
+    @application.user = current_user
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def booking_params
+    params.require(:application).permit(:requirements)
+  end
+
+  def set_pet
+    @pet = Pet.find(params[:pet_id])
+  end
 end
