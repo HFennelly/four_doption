98115ba6f34785ff49df6d115561552a0c309a72
Lois Graham <loisgraham12@gmail.com>
2022-09-06 13:02:29 +0100

    Added new and create method and created new view

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,4 +10,11 @@ class PetsController < ApplicationController
   def new
     @pet = Pet.new
   end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    @pet.save
+    redirect_to pets_path
+  end
 end
