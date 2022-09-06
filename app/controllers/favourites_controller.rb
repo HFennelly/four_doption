3f067370c3cbd44cc56b923a9d2b5b685ec63779
Blake McKenzie <blakemckenzie365@gmail.com>
2022-09-06 17:47:43 +0100

    favourites index method added, delete method needed for favourites

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,2 +1,18 @@
 class FavouritesController < ApplicationController
+  def index
+    @favourites = Favourite.all
+  end
+
+  def destroy
+    @favourite = Favourite.find(params[:id])
+    # @pet = @favourite.pet
+    @favourite.destroy
+    # redirect_to pet_path(@pet), status: :see_other
+  end
+
+  def favourite_params
+    params.require(:favourite)
+  end
+  # for tommorow
+  # need to be able to delete a favourite from a list of favourites (there is no favourites show page here)
 end
