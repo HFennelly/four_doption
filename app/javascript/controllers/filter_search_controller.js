d6e4e2a3107cd82ed70c9e2017916207a70644a8
Katie Hulme <katiehulme26@hotmail.com>
2022-09-08 15:18:13 +0100

    deleted code in stimuli that wasn't being used

--- app/javascript/controllers/filter_search_controller.js
+++ app/javascript/controllers/filter_search_controller.js
@@ -19,55 +19,5 @@ export default class extends Controller {
     buttonHide.classList.remove("d-none")
   }
 
-  refine(e) {
-    // console.log(this.speciesTarget.value)
-    // console.log(this.sexTarget.value)
-    // console.log(e.target.attributes.id === 'Species')
-    // console.log(e.target.attributes.id.value === 'Species')
-    const species = this.speciesTarget.value.toLowerCase()
-    const sex = this.sexTarget.value.toLowerCase()
-    console.log(sex);
-    console.log(species);
-    const size = this.sizeTarget.value.toLowerCase()
-    // console.log(size)
-    // console.log(species)
 
-
-    this.petTargets.forEach((pet) => {
-      console.log(pet)
-      console.log(pet.dataset.sex === sex)
-      console.log(pet.dataset.sex)
-
-          if ((pet.dataset.sex === sex) || (pet.classList.contains("d-none") && pet.dataset.sex === sex)) {
-            pet.classList.remove("d-none")
-            if (pet.dataset.species === species || pet.classList.contains("d-none") && pet.dataset.species === species) {
-              pet.classList.remove("d-none")
-            } else if (pet.dataset.species !== species && pet.dataset.sex !== sex) {
-              pet.classList.add("d-none")
-            } else { pet.classList.add("d-none")}
-          } else {
-            pet.classList.add("d-none")
-          }
-        })
-
-  }
 }
-
-
-
-
-      //   if (pet.dataset.sex == sex && pet.dataset.species == species) {
-      //         pet.parentElement.remove('d-none')
-      //    } else if (pet.dataset.sex == sex) {
-      //   pet.classList.remove('d-none')
-      //   } else if (pet.dataset.species != species) {
-      //     pet.classList.add('d-none')
-      //   } else if (pet.dataset.sex != sex) {
-      //   pet.classList.add('d-none') }
-      //   else {
-
-      //   }
-
-
-      // })
-      // }
