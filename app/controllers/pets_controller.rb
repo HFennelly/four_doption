ef6042ffe6b5fe408939f50835d72f79d1f28cf9
Katie Hulme <katiehulme26@hotmail.com>
2022-09-09 11:54:03 +0100

    added location to filter

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -29,6 +29,14 @@ class PetsController < ApplicationController
       @pets = @pets.where(size: params[:size])
     end
 
+    if params[:location] != "" && params[:location].present?
+      sql_query = <<~SQL
+        location Ilike :location
+      SQL
+      @pets = @pets.where(sql_query, location: "%#{params[:location]}%")
+    end
+
+
     if params.keys.count > 2
       @searching = true
     else
